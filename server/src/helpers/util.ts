68b856dae487aa8dbe15bed5a3a57a1b6e986999
Dhilip Buddha <dhilip.buddha@blackrock.com>
2020-06-28 13:01:50 +0100

    Types and indentation

--- server/src/helpers/util.ts
+++ server/src/helpers/util.ts
@@ -1,25 +1,19 @@
-import * as crypto from 'crypto';
-
 export class Util {
-	public static randomBetween(min, max) {
-		return Math.floor(Math.random() * (max - min + 1) + min);
-	}
+  public static randomBetween(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-	public static randomFrom(array) {
-		return array[this.randomBetween(0, array.length - 1)];
-	}
+  public static randomFrom(array: any[]): any {
+    return array[this.randomBetween(0, array.length - 1)];
+  }
 
-	public static minutesToMs(minute) {
-		return minute * 60 * 1000;
-	}
+  public static minutesToMs(minute: number): number {
+    return minute * 60 * 1000;
+  }
 
-	public static hash(stringToHash) {
-		return crypto.createHash('md5').update(stringToHash).digest('hex');
-	}
+  public static getDecimalPlaces(num: number): number {
+    return ((num + '').split('.')[1] || []).length;
+  }
 
-	public static getDecimalPlaces(num) {
-		return ((num + "").split(".")[1] || []).length;
-	}
-
-};
+}
 
--- server/src/scraper-util.ts
+++ server/src/scraper-util.ts
@@ -9,116 +9,116 @@ import { SimplePost, UserFeedResponseItem } from './models';
 
 @injectable()
 export class ScraperUtil {
-	private readonly logger: Logger = new Logger(this);
+  private readonly logger: Logger = new Logger(this);
 
-	constructor(
-		private instagram: InstagramClient,
-		private firebaseClient: FirebaseClient
-	) { }
+  constructor(
+    private instagram: InstagramClient,
+    private firebaseClient: FirebaseClient
+  ) { }
 
-	public transformPost(post: UserFeedResponseItem | TagFeedResponseItemsItem | LocationFeedResponseMedia | LocationFeedResponseMedia): SimplePost {
-		if (!has(post, 'location')) {
-			return null;
-		}
-		if (!has(post, 'image_versions2.candidates[0].url')) {
-			return null;
-		}
+  public transformPost(post: UserFeedResponseItem | TagFeedResponseItemsItem | LocationFeedResponseMedia | LocationFeedResponseMedia): SimplePost {
+    if (!has(post, 'location')) {
+      return null;
+    }
+    if (!has(post, 'image_versions2.candidates[0].url')) {
+      return null;
+    }
 
-		const simplifiedPost = {
-			...post,
-			code: post.code,
-			caption: post.caption?.text || '',
-			location: { latitude: +post.location.lat, longitude: +post.location.lng },
-			user: {
-				name: post.user.username,
-				full_name: post.user.full_name,
-				pic: post.user.profile_pic_url,
-			},
-		};
+    const simplifiedPost = {
+      ...post,
+      code: post.code,
+      caption: post.caption?.text || '',
+      location: { latitude: +post.location.lat, longitude: +post.location.lng },
+      user: {
+        name: post.user.username,
+        full_name: post.user.full_name,
+        pic: post.user.profile_pic_url,
+      },
+    };
 
-		return simplifiedPost;
-	}
+    return simplifiedPost;
+  }
 
-	async storePosts(posts: SimplePost[]): Promise<void> {
-		const storedPosts = (await this.firebaseClient.posts.get()) || {};
-		for (const post of posts) {
-			const isInFirebase = Object.keys(storedPosts).includes(post.code);
-			if (isInFirebase) {
-				this.logger.log(`${post.code} already exists`);
-				continue;
-			}
-			await this.firebaseClient.posts.add(post.code, post);
-		}
-	}
+  async storePosts(posts: SimplePost[]): Promise<void> {
+    const storedPosts = (await this.firebaseClient.posts.get()) || {};
+    for (const post of posts) {
+      const isInFirebase = Object.keys(storedPosts).includes(post.code);
+      if (isInFirebase) {
+        this.logger.log(`${post.code} already exists`);
+        continue;
+      }
+      await this.firebaseClient.posts.add(post.code, post);
+    }
+  }
 
-	async getByUser(user: string): Promise<UserFeedResponseItem[]> {
-		this.logger.log(user, 'getByUser');
-		const account = await this.instagram.client.user.searchExact(user);
-		if (account) {
-			const feed = await this.instagram.feed.user(account.pk);
+  async getByUser(user: string): Promise<UserFeedResponseItem[]> {
+    this.logger.log(user, 'getByUser');
+    const account = await this.instagram.client.user.searchExact(user);
+    if (account) {
+      const feed = await this.instagram.feed.user(account.pk);
 
-			// Get pages to scrape
-			let pagesToScrape = 5;
-			const usersScraped = (await this.firebaseClient.users.get()) || {};
-			await this.firebaseClient.users.add(user, { lastScraped: new Date(), ...account });
-			if (!user.includes(keys(usersScraped))) {
-				pagesToScrape = 1;
-			}
+      // Get pages to scrape
+      let pagesToScrape = 5;
+      const usersScraped = (await this.firebaseClient.users.get()) || {};
+      await this.firebaseClient.users.add(user, { lastScraped: new Date(), ...account });
+      if (!user.includes(keys(usersScraped))) {
+        pagesToScrape = 1;
+      }
 
-			const posts: UserFeedResponseItem[] = [];
-			for (let i = 0; i < pagesToScrape; i++) {
-				posts.push(...(await feed.items() as UserFeedResponseItem[]));
-			};
-			const filteredPosts = posts.filter(InstagramUtil.isValidImage);
-			this.logger.log(user, 'Retrieved', filteredPosts.length, 'items');
-			return filteredPosts;
-		}
+      const posts: UserFeedResponseItem[] = [];
+      for (let i = 0; i < pagesToScrape; i++) {
+        posts.push(...(await feed.items() as UserFeedResponseItem[]));
+      }
+      const filteredPosts = posts.filter(InstagramUtil.isValidImage);
+      this.logger.log(user, 'Retrieved', filteredPosts.length, 'items');
+      return filteredPosts;
+    }
 
-		return [];
-	}
+    return [];
+  }
 
-	async getByHashtag(hashtag: string): Promise<TagFeedResponseItemsItem[]> {
-		this.logger.log(hashtag, 'getByHashtag');
-		const feed = await this.instagram.feed.tags(hashtag, 'top');
+  async getByHashtag(hashtag: string): Promise<TagFeedResponseItemsItem[]> {
+    this.logger.log(hashtag, 'getByHashtag');
+    const feed = await this.instagram.feed.tags(hashtag, 'top');
 
-		// Get pages to scrape
-		let pagesToScrape = 5;
-		const tagsScraped = (await this.firebaseClient.tags.get()) || {};
-		await this.firebaseClient.tags.add(hashtag, { lastScraped: new Date() });
-		if (!hashtag.includes(keys(tagsScraped))) {
-			pagesToScrape = 1;
-		}
+    // Get pages to scrape
+    let pagesToScrape = 5;
+    const tagsScraped = (await this.firebaseClient.tags.get()) || {};
+    await this.firebaseClient.tags.add(hashtag, { lastScraped: new Date() });
+    if (!hashtag.includes(keys(tagsScraped))) {
+      pagesToScrape = 1;
+    }
 
-		const posts: TagFeedResponseItemsItem[] = [];
-		for (let i = 0; i < pagesToScrape; i++) {
-			posts.push(...(await feed.items() as TagFeedResponseItemsItem[]));
-		};
-		const filteredPosts = posts.filter(post => InstagramUtil.isValidImage(post) && InstagramUtil.hasHighLikeCount(post));
-		this.logger.log(hashtag, 'Retrieved', filteredPosts.length, 'items');
-		return filteredPosts;
-	}
+    const posts: TagFeedResponseItemsItem[] = [];
+    for (let i = 0; i < pagesToScrape; i++) {
+      posts.push(...(await feed.items() as TagFeedResponseItemsItem[]));
+    }
+    const filteredPosts = posts.filter(post => InstagramUtil.isValidImage(post) && InstagramUtil.hasHighLikeCount(post));
+    this.logger.log(hashtag, 'Retrieved', filteredPosts.length, 'items');
+    return filteredPosts;
+  }
 
-	async getByLocation(locationId: string): Promise<LocationFeedResponseMedia[]> {
-		this.logger.log(locationId, 'getByLocation');
-		locationId = locationId + '';
-		const feed = await this.instagram.feed.location(locationId, 'ranked');
+  async getByLocation(locationId: string): Promise<LocationFeedResponseMedia[]> {
+    this.logger.log(locationId, 'getByLocation');
+    locationId = locationId + '';
+    const feed = await this.instagram.feed.location(locationId, 'ranked');
 
-		// Get pages to scrape
-		let pagesToScrape = 5;
-		const tagsScraped = (await this.firebaseClient.locations.get()) || {};
-		await this.firebaseClient.locations.add(locationId, { lastScraped: new Date() });
-		if (!locationId.includes(keys(tagsScraped))) {
-			pagesToScrape = 1;
-		}
+    // Get pages to scrape
+    let pagesToScrape = 5;
+    const tagsScraped = (await this.firebaseClient.locations.get()) || {};
+    await this.firebaseClient.locations.add(locationId, { lastScraped: new Date() });
+    if (!locationId.includes(keys(tagsScraped))) {
+      pagesToScrape = 1;
+    }
 
-		const posts: LocationFeedResponseMedia[] = [];
-		for (let i = 0; i < pagesToScrape; i++) {
-			posts.push(...(await feed.items() as LocationFeedResponseMedia[]));
-		};
-		const filteredPosts = posts.filter(post => InstagramUtil.isValidImage(post) && InstagramUtil.hasHighLikeCount(post));
-		this.logger.log(locationId, 'Retrieved', filteredPosts.length, 'items');
-		return filteredPosts;
-	}
+    const posts: LocationFeedResponseMedia[] = [];
+    for (let i = 0; i < pagesToScrape; i++) {
+      posts.push(...(await feed.items() as LocationFeedResponseMedia[]));
+    }
+    const filteredPosts = posts.filter(post => InstagramUtil.isValidImage(post) && InstagramUtil.hasHighLikeCount(post));
+    this.logger.log(locationId, 'Retrieved', filteredPosts.length, 'items');
+    return filteredPosts;
+  }
 
 }
 
