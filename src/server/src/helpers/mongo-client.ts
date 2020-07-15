256e9532d46f5caa94f512368403c79ade79d3d4
Dhilip Buddha <dhilip.buddha@blackrock.com>
2020-07-15 13:30:54 +0100

    Add back monogo client

---
+++ src/server/src/helpers/mongo-client.ts
@@ -0,0 +1,39 @@
+import * as Mongoose from 'mongoose';
+import { injectable } from 'tsyringe';
+
+import { Config } from './config';
+import { Logger } from './logger';
+
+@injectable()
+export class MongoClient {
+
+  private readonly logger: Logger = new Logger(this);
+
+  public database: Mongoose.Connection;
+
+  public connect(): void {
+    if (this.database) {
+      return;
+    }
+
+    Mongoose.connect(Config.MongoDb.Credentials, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    this.database = Mongoose.connection;
+    this.database.once("open", async () => {
+      this.logger.log("Connected to database");
+    });
+    this.database.on("error", e => {
+      this.logger.log("Error connecting to database", e);
+    });
+  }
+
+  public disconnect(): void {
+    Mongoose.disconnect();
+  }
+
+}
